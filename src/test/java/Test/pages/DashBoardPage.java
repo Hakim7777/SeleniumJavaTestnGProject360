c5102eb03bcf933e6fcf2a8e42dc674cb287e451
Eliot <90345161+Hakim7777@users.noreply.github.com>
2025-06-14 20:28:24 +0100

    Update DashBoardPage.java

--- src/test/java/Test/pages/DashBoardPage.java
+++ src/test/java/Test/pages/DashBoardPage.java
@@ -1,28 +1,24 @@
-package Test.pages;
-
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class DashBoardPage {
-	WebDriver driver;
-	//constructor
-	public DashBoardPage(WebDriver lDriver)
-	{
-		this.driver=lDriver;
-		
-		PageFactory.initElements(driver,this);
-	}
-	
-	@FindBy(xpath="//*[@id=\"login-list\"]/li[1]/a") WebElement dash ;
-	@FindBy(xpath ="//*[@id=\"learn-press-user-profile\"]/ul/li[3]/a") WebElement offerAcademie;
-	@FindBy(xpath = "//*[@id=\"tab-academies\"]/div/div/ul/li[1]/form/div/button")WebElement SubscribeButton;
+    WebDriver driver;
+    WebDriverWait wait;
 
-	public void dashboardclick()
-	{
-		dash.click();
-		offerAcademie.click();
-		SubscribeButton.click();
-		}
+    public DashBoardPage(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    public void dashboardclick() {
+        WebElement dashboardLink = wait.until(
+            ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='login-list']/li[1]/a"))
+        );
+        dashboardLink.click();
+    }
 }
